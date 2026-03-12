c860b0a134f72d87b55b9d16705d0a7e58c27f62
abhis <565105@mail.muni.cz>
2026-03-12 01:00:15 +0100

    Discard Performance Editor Plan + Fixed Reference Issue on DA of Camera Actor

--- Plugins/UpStage/Source/UpStage/Private/UpStageLabanMovementModifier.cpp
+++ Plugins/UpStage/Source/UpStage/Private/UpStageLabanMovementModifier.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+#include "AnimPose.h"
 #include "UpStageLabanMovementModifier.h"
 #include "UpStageLabanMovementAnalysisType.h"
 #include "UpStageJointData.h"
