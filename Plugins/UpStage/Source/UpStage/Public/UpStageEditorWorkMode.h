c1df74e9f70e08ed0ee026ca6b7d0086b6c98449
abhis <565105@mail.muni.cz>
2026-03-25 16:54:24 +0100

    Safeguard Frame Evaluation Operation

--- Plugins/UpStage/Source/UpStage/Public/UpStageEditorWorkMode.h
+++ Plugins/UpStage/Source/UpStage/Public/UpStageEditorWorkMode.h
@@ -10,5 +10,6 @@ enum class EUpStageEditorWorkMode : uint8
 	Complete UMETA(DisplayName = "Complete"),
 	KeyMomentSelection UMETA(DisplayName = "Key Moment Selection"),
 	CameraSelection UMETA(DisplayName = "Camera Selection"),
-	QueueRenders UMETA(DisplayName = "Queue Renders")
+	QueueRenders UMETA(DisplayName = "Queue Renders"),
+	Cancel UMETA(DisplayName = "Cancel")
 };
